58c425908cbd55ee2733398226b931ee3be9273d
iAMMIA <iammia4tp@gmail.com>
2025-03-24 01:59:23 +0700

    complete 70%

--- client/src/router/Detection.js
+++ client/src/router/Detection.js
@@ -1,55 +1,66 @@
 import './css/Detection.css';
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useDarkMode } from './DarkModeContext';
 import axios from 'axios';
 
 function Detection() {
-  const {darkMode} = useDarkMode();
+  const { darkMode } = useDarkMode();
   const [drugInfo, setDrugInfo] = useState([]);
   const [imageLink, setImageLink] = useState(null);
   const [detailDrugInfo, setDetailDrugInfo] = useState(null);
-  const [currentFrameDetected, setCurrentFrameDetected] = useState(null);
-  
+  // const [currentFrame, setCurrentFrame] = useState(null); // Theo dõi frame hiện tại để làm key
+  const currentFrameRef = useRef(null); // Dùng useRef để lưu giá trị trước đó
+
   useEffect(() => {
     const socket = new WebSocket("ws://jetson-xavier-nx.tailnet-8188.ts.net:8765");
 
-    // Khi kết nối mở
     socket.onopen = () => {
-        // alert("Connected to WebSocket Server");
+      // alert("Connected to WebSocket Server");
     };
 
-    // Khi nhận được dữ liệu từ Python
     socket.onmessage = (event) => {
       try {
         const data = JSON.parse(event.data);
-
-
+        console.log("Dữ liệu nhận được:", data.frame);
+        
+        // Nếu frame_detected thay đổi, cập nhật currentFrame để reload component
+        if (data.frame !== undefined && data.frame !== currentFrameRef.current) {
+          // setCurrentFrame(data.frame);
+          currentFrameRef.current = data.frame;
+          console.log("setCurrentFrame:", data.frame);
+          console.log("CurrentFrame:", currentFrameRef.current);
+          setDrugInfo([]); // Reset drugInfo về rỗng
+          setImageLink(null); // Reset imageLink về null
+        } 
+        
         if (data.name !== undefined && data.count !== undefined && data.image_bounding_box !== undefined) {
-          // setDrugInfo((prevData) => [...prevData, data]); 
+          
           setDrugInfo((prevData) => {
-            // Tìm xem đã có drug với tên này chưa
             const existingDrugIndex = prevData.findIndex(drug => drug.name === data.name);
             if (existingDrugIndex !== -1) {
-              // Nếu đã tồn tại, thêm image_bounding_box vào mảng bounding_boxes
               const updatedDrug = {
                 ...prevData[existingDrugIndex],
-                count: prevData[existingDrugIndex].count,
+                count: data.count,
                 bounding_boxes: [...prevData[existingDrugIndex].bounding_boxes, data.image_bounding_box]
               };
               const newData = [...prevData];
               newData[existingDrugIndex] = updatedDrug;
               return newData;
             } else {
-              // Nếu chưa tồn tại, thêm drug mới vào mảng
               return [...prevData, {
-                  name: data.name,
-                  count: data.count,
-                  bounding_boxes: [data.image_bounding_box]
-                }];
+                name: data.name,
+                count: data.count,
+                bounding_boxes: [data.image_bounding_box]
+              }];
             }
           });
         }
-        setImageLink(`data:image/jpeg;base64,${data.image}`); // Hiển thị hình ảnh từ base64
+
+        // Cập nhật hình ảnh chính
+        if (data.image) {
+          setImageLink(`data:image/jpeg;base64,${data.image}`);
+        }
+
       } catch (error) {
         console.error("Error parsing JSON:", error);
       }
@@ -57,118 +68,97 @@ function Detection() {
 
     window.scrollTo(0, 0);
 
-    // Khi kết nối bị đóng
     socket.onclose = () => {
-        // alert("WebSocket connection closed");
+      // alert("WebSocket connection closed");
     };
 
-    // Khi có lỗi xảy ra
     socket.onerror = (error) => {
-        // alert("WebSocket Error:", error);
+      // alert("WebSocket Error:", error);
     };
 
     return () => {
-      socket.close(); // Ngắt kết nối khi component unmount
-    }
-  },[])
+      socket.close();
+    };
+  }, []); // Dependency array rỗng để effect chỉ chạy một lần khi mount
 
-  const get_detail_drug_info = (nameDrug) =>{
+  const get_detail_drug_info = (nameDrug) => {
     axios.get(`http://localhost:3001/get-name-drug/${nameDrug}`)
-        .then(response => {
-            if (response.data && response.data.length > 0) {
-              setDetailDrugInfo(response.data[0]);
-          } else {
-              setDetailDrugInfo(null);
-          }
-        })
-        .catch(error => {
-            console.error("Error fetching drug details:", error);
-        });
-  }
+      .then(response => {
+        if (response.data && response.data.length > 0) {
+          setDetailDrugInfo(response.data[0]);
+        } else {
+          setDetailDrugInfo(null);
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching drug details:", error);
+      });
+  };
 
   return (
-      <div className='Detection'>
-        <div className={`detection_container`}>
-          <div className="d_result" style={{ maxHeight: '400px', overflowY: 'auto' }}>
-            {drugInfo.length > 0 ? (
-              <div className='main_result'>
-                {drugInfo.map((drug, index) => (
+    <div className='Detection'>
+      <div className={`detection_container`}>
+        <div className="d_result" style={{ maxHeight: '400px', overflowY: 'auto' }}>
+          {drugInfo.length > 0 ? (
+            // Sử dụng key dựa trên currentFrame để reload main_result khi frame thay đổi
+            <div className='main_result'>
+              {drugInfo.map((drug, index) => (
                 <div key={index} className='drug_item'>
                   <div className='basic_drug_info' onClick={() => get_detail_drug_info(drug.name)}>
                     <div className='drug_bounding_box'>
                       {drug.bounding_boxes.map((boundingBox, idx) => (
-                          <div key={idx} className='drug_bounding_box'>
-                            <img src={`data:image/jpeg;base64,${boundingBox}`} alt="Drug" style={{ width: '100%', height: 'auto' }} />
-                          </div>
-                        ))}                  
-                      {/* <img src={`data:image/jpeg;base64,${drug.image_bounding_box}`} alt="Drug" style={{ width: '100%', height: 'auto' }} /> */}
+                        <div key={idx} className='drug_bounding_box'>
+                          <img src={`data:image/jpeg;base64,${boundingBox}`} alt="Drug" style={{ width: '100%', height: 'auto' }} />
+                        </div>
+                      ))}
                     </div>
                     <div className='drug_info'>
-                        <p><strong>Tên thuốc:</strong> {drug.name}</p>
-                        <p><strong>Số lượng:</strong> {drug.count}</p>
-                        <hr/>
+                      <p><strong>Tên thuốc:</strong> {drug.name}</p>
+                      <p><strong>Số lượng:</strong> {drug.count}</p>
+                      <hr />
                     </div>
                   </div>
                 </div>
-                ))}
-              </div>
-            ):(
-              <div className='intro_result'>
-                <div className={`intro_result1 ${darkMode ? 'dark_mode':''}`}>
-                  <p>Bạn có thể tra cứu hình ảnh thuốc ở đây.</p>
-                </div>
-              </div>
-            )}
-          </div>
-          <div className = "show_input_video">
-              {imageLink && (
-                <div>
-                  <img src={imageLink} alt="Drug" style={{ width: '100%', height: 'auto' }} />
-                </div>
-              )}
-          </div>
-        </div>
-        <div className="detection_detail">
-          {/* <div className='drug_detail'>
-            <h3>Thông tin chi tiết:</h3>
-            <p><strong>Tên thuốc:</strong></p>
-
-            <div>
-              <strong>Công dụng:</strong>
+              ))}
             </div>
-
-            <div>
-              <strong>Thành phần:</strong>
-            </div>
-
-            <div>
-              <strong>Tác dụng phụ:</strong>
-            </div>
-          </div> */}
-
-          {detailDrugInfo && (
-            <div className='drug_detail'>
-              <h3>Thông tin chi tiết:</h3>
-              <p><strong>Tên thuốc:</strong> {detailDrugInfo.name}</p>
-
-              <div>
-                <strong>Công dụng:</strong>
-                <div dangerouslySetInnerHTML={{ __html: detailDrugInfo.uses }} />
-              </div>
-
-              <div>
-                <strong>Thành phần:</strong>
-                <div dangerouslySetInnerHTML={{ __html: detailDrugInfo.excipients }} />
-              </div>
-
-              <div>
-                <strong>Tác dụng phụ:</strong>
-                <div dangerouslySetInnerHTML={{ __html: detailDrugInfo.side_effects }} />
+          ) : (
+            <div className='intro_result'>
+              <div className={`intro_result1 ${darkMode ? 'dark_mode' : ''}`}>
+                <p>Bạn có thể tra cứu hình ảnh thuốc ở đây.</p>
               </div>
             </div>
           )}
         </div>
+        <div className="show_input_video">
+          {imageLink && (
+            <div>
+              <img src={imageLink} alt="Drug" style={{ width: '100%', height: 'auto' }} />
+            </div>
+          )}
+        </div>
       </div>
+      <div className="detection_detail">
+        {detailDrugInfo && (
+          <div className='drug_detail'>
+            <h3>Thông tin chi tiết:</h3>
+            <p><strong>Tên thuốc:</strong> {detailDrugInfo.name}</p>
+            <div>
+              <strong>Công dụng:</strong>
+              <div dangerouslySetInnerHTML={{ __html: detailDrugInfo.uses }} />
+            </div>
+            <div>
+              <strong>Thành phần:</strong>
+              <div dangerouslySetInnerHTML={{ __html: detailDrugInfo.excipients }} />
+            </div>
+            <div>
+              <strong>Tác dụng phụ:</strong>
+              <div dangerouslySetInnerHTML={{ __html: detailDrugInfo.side_effects }} />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
+
 export default Detection;
